7760b4ec8139856b73a083c3b401f853fb52f216
Bavithbabu <bavithbabu25@gmail.com>
2025-12-15 23:21:57 +0530

    Initial commit: Go JWT Authentication

---
+++ routes/userRoutes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"go-lang-jwt/controllers"
+	"go-lang-jwt/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controllers.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controllers.GetUser())
+}
